6adc84f873398871fd3d1a6bfd1f9ff190955ef4
John Stevenson <john@jr0cket.com>
2010-07-16 11:35:30 +0100

    Code from dojo 2 - started test rewrite

--- minesweeper-dojo/src/main/scala/com/assembla/lsug/dojo/minesweeper/Board.scala
+++ minesweeper-dojo/src/main/scala/com/assembla/lsug/dojo/minesweeper/Board.scala
@@ -1,19 +1,43 @@
 package com.assembla.lsug.dojo.minesweeper
 
 object Board {
-	type Grid = List[List[Int]]
-	def transform(s:String) = {
-		"*100\n2210\n1*10\n1110"
+	type Grid = List[List[Cell]]
+	def transform(s:String) : String = {
 
+   // val outputString = new scala.collection.mutable.ListBuffer[String]
+    val grid = parse(s)
+//    val listOfInt: List[Int] = grid flatMap
+    
+    val slots: List[String] = for {row <- grid } yield codeFor(row)
+
+    slots reduceLeft({_+_})
+/*     grid.foreach(row => {
+      row.foreach(slot =>
+        outputString += codeFor(slot))
+    })
+    outputString.reduce("", {_+_})
+*/
+    //outputString
 	}
+
+  def codeFor(row : List[Cell]):String = row match {
+    case cell::rest if cell.isMine => "*" + codeFor(rest)
+    case _::rest => "0" + codeFor(rest)
+    case _  => ""
+  }
 
 	def parse(in: String): Grid = {
 		def parseRow(row: String) = {
 			row.map {
-				case '*' => 1
-				case _   => 0
+				case '*' => new Cell(true)
+				case _   => new Cell(false)
 			} toList
 		}
 		in.split('\n').map(parseRow _).toList
 	}
+}
+
+class Cell(val isMine: Boolean) {
+
+
 }
--- minesweeper-dojo/src/test/scala/com/assembla/lsug/dojo/minesweeper/BoardReaderTest.scala
+++ minesweeper-dojo/src/test/scala/com/assembla/lsug/dojo/minesweeper/BoardReaderTest.scala
@@ -1,8 +1,8 @@
 package com.assembla.lsug.dojo.minesweeper
 
-import org.junit.Test
 import org.junit.Assert.assertThat
 import org.hamcrest.CoreMatchers._
+import org.junit.{Ignore, Test}
 
 class BoardReaderTest {
 
@@ -22,6 +22,7 @@ class BoardReaderTest {
 		assertThat( output, is(equalTo (expectedBoard)))
 	}
 	@Test
+  @Ignore
 	def	testEmptyField(){
 
 		val input = """|....
@@ -40,23 +41,41 @@ class BoardReaderTest {
 
 	@Test
 	def testParsing() {
-		assertThat(Board.parse("*.."), is(equalTo (List(List(1, 0, 0)))))
+    val b = Board.parse("*..")
+		assertThat(b.length, is(equalTo (1)))
+    //assertThat(b.cols, is(equalTo (3)))
+    //assertThat(b.render, is(equalTo ("*10")))
 	}
 
-	@Test
-	def testParseTwoRows() {
-		assertThat(Board.parse("*..\n..."), is(equalTo (List(List(1, 0, 0), List(0, 0, 0)))))
- 	}
+/*  The following tests have been commented out as they represent how something can be coded
+    and not the intent of the tests.  It is recommnended that the following tests be written
+    in a similar approach to the above test in the next dojo.
+*/
 
-	@Test
-	def testEmptyRow {
-		val emptyRow = "...."
-		assertThat(Board.parse(emptyRow), is(equalTo(List(List(0, 0, 0, 0)))))
-	}
-
-	@Test
-	def testEmptyString {
-		val emptyString = ""
-		assertThat(Board.parse(emptyString), is(equalTo(List(List[Int]()))))
-	}
+//	@Test
+//	def testParseTwoRows() {
+//		assertThat(Board.parse("*..\n..."), is(equalTo (List(List(1, 0, 0), List(0, 0, 0)))))
+// 	}
+//
+//	@Test
+//	def testEmptyRow {
+//		val emptyRow = "...."
+//		assertThat(Board.parse(emptyRow), is(equalTo(List(List(0, 0, 0, 0)))))
+//	}
+//
+//	@Test
+//	def testEmptyString {
+//		val emptyString = ""
+//		assertThat(Board.parse(emptyString), is(equalTo(List(List[Int]()))))
+//	}
+//
+//  @Test
+//  def testTransformOfOneLine() {
+//    assertThat(Board.transform("..*."), is(equalTo("01*1")))
+//  }
+//
+//  @Test
+//  def testACellShouldSayItIsAMine() {
+//    assertThat(new Cell(true).isMine, is(true))
+//  }
 }
